5547c40cfa3c9cededda7b4fb6c065329209c481
tian_jianen <tian_jianen@venusgroup.com.cn>
2021-04-08 10:48:09 +0800

    修改文档

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,12 +3,32 @@
  * @Company: Venus
  * @Date: 2021-04-08 10:00:33
  * @LastEditors: Kian_
- * @LastEditTime: 2021-04-08 10:08:09
+ * @LastEditTime: 2021-04-08 10:47:15
  */
 module.exports = {
   title: 'Hello VitePress',
   description: 'Just playing around.',
+  editLink: true,
   markdown: {
     lineNumbers: true
-  }
+  },
+  $site:{
+    base: '/',
+    lang: 'en-US',
+    title: 'VitePress',
+    description: 'Vite & Vue powered static site generator.',
+    head: [],
+    locales: {},
+    themeConfig:{
+      locales: {},
+      repo: 'vuejs/vitepress',
+      docsDir: 'docs',
+      editLinks: true,
+      editLinkText: 'Edit this page on GitHub',
+      lastUpdated: 'Last Updated',
+      nav: true,
+      sidebar: 'auto'
+    }
+  },
+  
 }
---
+++ docs/test.md
@@ -0,0 +1,9 @@
+<!--
+ * @Version: 
+ * @Company: Venus
+ * @Date: 2021-04-08 10:37:41
+ * @LastEditors: Kian_
+ * @LastEditTime: 2021-04-08 10:37:50
+-->
+
+# hello VitePress
